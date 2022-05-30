6b1c87de4eeae04a4feb7255cf6c900aa4018854
Michael Crawford <webdevcrawford@gmail.com>
2022-05-29 20:53:59 -0600

    removes some instructional comments

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const resetButton = document.querySelector("#reset-button")
 // New To Do
 submitButton.addEventListener('click', function() {
   const newToDo = document.createElement('li')
+  // Don’t Allow Empty Items to be Added to the To-do List
   if (toDoInput.value !== "") {
     newToDo.textContent = toDoInput.value
     newToDo.className = 'todo-item'
@@ -31,12 +32,6 @@ resetButton.addEventListener('click', function() {
 
 
 // # Level Ups 🚀
-
-// ## Don’t Allow Empty Items to be Added to the To-do List
-// Right now, if a user clicks the `button` element without any text in the `input` field, an empty bullet point is created. Prevent this from happening.
-
-// ## Reset Button
-// Add a Reset button. After the Reset button is clicked all items in the to-do list should be removed. The input field should also be cleared so that it doesn’t hold any text.
 
 // ## Delete an Item on Click
 // We’ll learn a better way to manage the state in our applications tomorrow, but for now, add an event listener to an existing cached element reference and manipulate the DOM to remove individual items from the to-do list when we click on them.
